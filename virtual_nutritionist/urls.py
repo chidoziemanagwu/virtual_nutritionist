14cdad0c25752250c0e950e800d896b431081206
Mr. Managwu <39161025+chidoziemanagwu@users.noreply.github.com>
2024-09-24 11:58:52 +0100

    virtual nurtitionist

--- virtual_nutritionist/urls.py
+++ virtual_nutritionist/urls.py
@@ -1,30 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Virtual Nutritionist API",
-      default_version='v1',
-      description="API documentation for the Virtual Nutritionist app",
-      terms_of_service="https://www.example.com/terms/",
-      contact=openapi.Contact(email="contact@virtualnutritionist.com"),
-      license=openapi.License(name="MIT License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #swagger urls
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-   
-   #normal app urls
     path('', include('mealplanner.urls')),
 ]
-
